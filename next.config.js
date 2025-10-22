86acf645314ebbcb13dd2f3f1de3372d8f21443e
dreamingdiffusion <43732369+dreamingdiffusion@users.noreply.github.com>
2025-10-22 08:10:38 -0700

    Create next.config.js file

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['iili.io', 'files.catbox.moe', 'flagcdn.com', 'storage.googleapis.com'],
+  },
+}
